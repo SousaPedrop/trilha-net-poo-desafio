db8ce3f2c0f8421455e587893121e3f253149df9
SousaPedrop <sousapedro.work@gmail.com>
2022-10-13 14:14:03 -0300

    Desafio concluído e comentários TODO apagados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste do Nokia: ");
+Nokia n1 = new Nokia("40028922", "101010101", "Nokia1", 16);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Pou");
+
+Console.WriteLine("==============================");
+
+Console.WriteLine("Teste do Iphone: ");
+Iphone i1 = new Iphone("11999887766", "010101010", "Iphone1", 64);
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("PUBG");
